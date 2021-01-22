de94b90a8611e6b8c4f88d2c83f9265ff1a9759a
kevin Cotrina <kcotrinam92@gmail.com>
2021-01-22 17:05:39 -0500

    Fix linter issues

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,4 @@
-const library = []
+const library = [];
 
 export default function Book(title, author, pages, status) {
   this.title = title;
@@ -9,86 +9,89 @@ export default function Book(title, author, pages, status) {
 
 Book.prototype.addBook = function () {
   library.push(this);
-}
+};
 
 export const setLocalStorage = () => {
-  window.localStorage.setItem('library', JSON.stringify(library))
-}
+  window.localStorage.setItem('library', JSON.stringify(library));
+};
 
 export const getLocalStorage = () => {
-  const collection = JSON.parse(window.localStorage.getItem('library'))
-  
-  if ( collection != null ) {
-    collection.forEach(el => {
-      library.push(el)
-    })
-  }
-}
+  const collection = JSON.parse(window.localStorage.getItem('library'));
 
-export const render = () =>{
-  const render_collection =  document.querySelector('.books-container');
-  render_collection.innerHTML = '';
-  library.forEach((book, idx) => {
-    fewCard(book, idx)
-  })
-  deleteCard()
-  changeStatus()
-}
+  if (collection != null) {
+    collection.forEach(el => {
+      library.push(el);
+    });
+  }
+};
 
 const fewCard = (book, id) => {
-  const render_collection =  document.querySelector('.books-container');
-  let container =  document.createElement("div");
-  let title = document.createElement('h1');
-  let author   = document.createElement('h2');
-  let page = document.createElement('h4');
-  let statusContainer = document.createElement('span')
-  let deleteBtn = document.createElement('button')
+  const renderCollection = document.querySelector('.books-container');
+  const container = document.createElement('div');
+  const title = document.createElement('h1');
+  const author = document.createElement('h2');
+  const page = document.createElement('h4');
+  const statusContainer = document.createElement('span');
+  const deleteBtn = document.createElement('button');
 
-  container.id = id
-  deleteBtn.dataset.id = id
+  container.id = id;
+  deleteBtn.dataset.id = id;
 
   title.innerHTML = book.title;
   author.innerHTML = book.author;
   page.innerHTML = book.pages;
   statusContainer.innerHTML = book.status;
-  deleteBtn.textContent = 'Delete'
-  deleteBtn.classList.add('btn-delete')
-  statusContainer.classList.add('status')
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('btn-delete');
+  statusContainer.classList.add('status');
 
   container.appendChild(title);
   container.appendChild(author);
   container.appendChild(page);
   container.appendChild(statusContainer);
   container.appendChild(deleteBtn);
-  
-  render_collection.appendChild(container);
-}
+
+  renderCollection.appendChild(container);
+};
+
+
+const changeStatus = () => {
+  const statusCollection = document.querySelectorAll('.status');
+  statusCollection.forEach(status => {
+    status.addEventListener('click', e => {
+      if (e.target.innerHTML.toUpperCase() === 'READ') {
+        status.innerHTML = 'Not read';
+      } else {
+        status.innerHTML = 'Read';
+      }
+    });
+  });
+};
+
+
+const deleteCard = () => {
+  const deleteBtns = [...document.querySelectorAll('.btn-delete')];
+  deleteBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+      library.splice(e.target.dataset.id, 1);
+      setLocalStorage();
+      render();
+    });
+  });
+};
+
+
+export const render = () => {
+  const renderCollection = document.querySelector('.books-container');
+  renderCollection.innerHTML = '';
+  library.forEach((book, idx) => {
+    fewCard(book, idx);
+  });
+  deleteCard();
+  changeStatus();
+};
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  
-})
 
-const deleteCard = () => {
-  const deleteBtns = [...document.querySelectorAll('.btn-delete')]
-  deleteBtns.forEach(btn => {
-    btn.addEventListener('click', e => {
-      library.splice(e.target.dataset.id, 1)
-      setLocalStorage()
-      render()
-    })
-  })
-}
-
-const changeStatus = () => {
-  const statusCollection = document.querySelectorAll('.status')
-  statusCollection.forEach(status => {
-    status.addEventListener('click', e =>{
-      if ( e.target.innerHTML.toUpperCase() == 'READ') {
-        status.innerHTML = 'Not read'
-      } else {
-        status.innerHTML = 'Read'
-      }
-    })
-  })
-}
+});
--- src/main.js
+++ src/main.js
@@ -1,36 +1,35 @@
-import Book, { setLocalStorage, getLocalStorage, render } from './logic.js'
+import Book, { setLocalStorage, getLocalStorage, render } from './logic.js';
 
 const createBtn = document.querySelector('.create-btn');
 
+const selectedStatus = () => {
+  let value;
+  const statusChosen = [...document.getElementsByName('status')];
+  statusChosen.map(opt => {
+    if (opt.checked) {
+      value = opt.value;
+    }
+  });
+  return value;
+};
+
 createBtn.addEventListener('click', e => {
   e.preventDefault();
-  const title = document.querySelector('#title').value
-  const author = document.querySelector('#author').value
-  const pages = document.querySelector('#pages').value
-  const statusValue = selectedStatus()
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const statusValue = selectedStatus();
 
-  const newBook = new Book(title, author, pages, statusValue)
-  newBook.addBook()
-  setLocalStorage()
+  const newBook = new Book(title, author, pages, statusValue);
+  newBook.addBook();
+  setLocalStorage();
   render();
-})
+});
 
 window.addEventListener('DOMContentLoaded', () => {
-  getLocalStorage()
-  render()
-}) 
-
-
-const selectedStatus = () => {
-  let value
-  const statusChosen = [...document.getElementsByName('status')]
-  statusChosen.map(opt => {
-    if( opt.checked ) {
-      value =  opt.value
-    } 
-  })
-  return value
-}
+  getLocalStorage();
+  render();
+});
 
 // const value = selectedStatus()
 
