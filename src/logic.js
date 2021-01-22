077e470424b17f5966fd29cb8843d72de78d46b0
kevin Cotrina <kcotrinam92@gmail.com>
2021-01-22 16:57:39 -0500

    Add population to library array and delete functionality

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,10 @@
 const library = []
 
-export default function Book(title, author, pages) {
+export default function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.status = status;
 }
 
 Book.prototype.addBook = function () {
@@ -16,31 +17,78 @@ export const setLocalStorage = () => {
 
 export const getLocalStorage = () => {
   const collection = JSON.parse(window.localStorage.getItem('library'))
-  console.log(collection)
+  
+  if ( collection != null ) {
+    collection.forEach(el => {
+      library.push(el)
+    })
+  }
 }
 
 export const render = () =>{
   const render_collection =  document.querySelector('.books-container');
   render_collection.innerHTML = '';
-  library.forEach(book => {
-    fewCard(book)
+  library.forEach((book, idx) => {
+    fewCard(book, idx)
   })
+  deleteCard()
+  changeStatus()
 }
 
-const fewCard = (book) => {
-  console.log(book.pages);
+const fewCard = (book, id) => {
   const render_collection =  document.querySelector('.books-container');
-  var container =  document.createElement("div");
-  var title = document.createElement('h1');
-  var author   = document.createElement('h2');
-  var page = document.createElement('h4');
+  let container =  document.createElement("div");
+  let title = document.createElement('h1');
+  let author   = document.createElement('h2');
+  let page = document.createElement('h4');
+  let statusContainer = document.createElement('span')
+  let deleteBtn = document.createElement('button')
+
+  container.id = id
+  deleteBtn.dataset.id = id
+
   title.innerHTML = book.title;
   author.innerHTML = book.author;
   page.innerHTML = book.pages;
+  statusContainer.innerHTML = book.status;
+  deleteBtn.textContent = 'Delete'
+  deleteBtn.classList.add('btn-delete')
+  statusContainer.classList.add('status')
+
   container.appendChild(title);
   container.appendChild(author);
   container.appendChild(page);
+  container.appendChild(statusContainer);
+  container.appendChild(deleteBtn);
+  
   render_collection.appendChild(container);
 }
 
 
+window.addEventListener('DOMContentLoaded', () => {
+  
+})
+
+const deleteCard = () => {
+  const deleteBtns = [...document.querySelectorAll('.btn-delete')]
+  deleteBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+      library.splice(e.target.dataset.id, 1)
+      setLocalStorage()
+      render()
+    })
+  })
+}
+
+const changeStatus = () => {
+  const statusCollection = document.querySelectorAll('.status')
+  statusCollection.forEach(status => {
+    status.addEventListener('click', e =>{
+      if ( e.target.innerHTML.toUpperCase() == 'READ') {
+        status.innerHTML = 'Not read'
+      } else {
+        status.innerHTML = 'Read'
+      }
+    })
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Book, { setLocalStorage,render } from './logic.js'
+import Book, { setLocalStorage, getLocalStorage, render } from './logic.js'
 
 const createBtn = document.querySelector('.create-btn');
 
@@ -7,10 +7,31 @@ createBtn.addEventListener('click', e => {
   const title = document.querySelector('#title').value
   const author = document.querySelector('#author').value
   const pages = document.querySelector('#pages').value
+  const statusValue = selectedStatus()
 
-  const newBook = new Book(title, author, pages)
+  const newBook = new Book(title, author, pages, statusValue)
   newBook.addBook()
   setLocalStorage()
   render();
 })
 
+window.addEventListener('DOMContentLoaded', () => {
+  getLocalStorage()
+  render()
+}) 
+
+
+const selectedStatus = () => {
+  let value
+  const statusChosen = [...document.getElementsByName('status')]
+  statusChosen.map(opt => {
+    if( opt.checked ) {
+      value =  opt.value
+    } 
+  })
+  return value
+}
+
+// const value = selectedStatus()
+
+// console.log(value)
